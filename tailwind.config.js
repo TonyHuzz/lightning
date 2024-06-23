54ac846f787af30d3727effeb605d740b7046321
TonyHu <benabcd5779@gmail.com>
2024-06-24 00:23:48 +0800

    feat: 安裝和設定 tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,21 @@ export default {
     ],
 
     theme: {
+        container: {
+            center: true,
+            padding: {
+                default: '1rem',
+                sm: '2rem',
+                lg: '3rem',
+                xl: '4rem',
+            },
+        },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Inter var', 'Noto Sans TC', ...defaultTheme.fontFamily.sans],
+                mono: [...defaultTheme.fontFamily.mono, 'Inter var', 'Noto Sans TC']
             },
         },
     },
-
     plugins: [forms],
 };
