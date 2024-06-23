848ef0996cff4e8b9d4d20cc727710c3ccb0e4eb
TonyHu <benabcd5779@gmail.com>
2024-06-23 23:56:54 +0800

    feat: 新增測試頁面

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div>
+            <Link href="/">Hello world</Link>
+            <Link href="/about">About</Link>
+        </div>
+        <h1>About</h1>
+    </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+</script>
---
+++ resources/js/Pages/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+    <Head title="hello world123" />
+
+    <div>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+    </div>
+    <h1>Hello {{ name }}</h1>
+</template>
+
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    name: {
+        type: String,
+        default: 'world',
+    },
+})
+
+</script>
+
+<style scoped>
+
+</style>
