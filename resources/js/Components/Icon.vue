32aa4b40ac97c8925eefc7876ff00a0781058e0c
TonyHu <benabcd5779@gmail.com>
2024-06-24 01:27:15 +0800

    feat: 製作 Icon Component

---
+++ resources/js/Components/Icon.vue
@@ -0,0 +1,18 @@
+<template>
+    <span :class="['iconify', style]" :data-icon="icon"></span>
+</template>
+
+<script setup>
+
+defineProps({
+    style: {
+        type: String,
+        default: "",
+    },
+    icon: {
+        type: String,
+        required: true
+    },
+})
+
+</script>
