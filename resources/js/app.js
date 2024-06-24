a95f06a889ab8448f6f10cf77058c4b5e7f08b57
TonyHu <benabcd5779@gmail.com>
2024-06-24 20:57:55 +0800

    feat: 定義專案的預設 Layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,14 +4,20 @@ import '@purge-icons/generated';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import AppLayout from "@/Layouts/AppLayout.vue";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) => {
+        // 設置每個頁面的layout 都會吃 AppLayout.vue
+        const pages = import.meta.glob('./Pages/**/*.vue', {eager: true});
+        let page = pages[`./Pages/${name}.vue`];
+        page.default.layout = page.default.layout || AppLayout;
+        return page;
+    },
     setup({ el, App, props, plugin }) {
         const app = createApp({ render: () => h(App, props) })
             .use(plugin)
