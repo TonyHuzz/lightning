8b799463175c22591126641c0d7019db9f2b5803
TonyHu <benabcd5779@gmail.com>
2024-06-24 13:31:27 +0800

    feat: 設定讓Vue可以使用 $route

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,10 +13,20 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        const app = createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .mount(el);
+
+        // 可以在Vue组件中使用$route方法
+        app.mixin({
+            methods: {
+                $route: (name, params, absolute, config = Ziggy) => route(name, params, absolute, config),
+            },
+        });
+
+        app.mount(el);
+
+
     },
     progress: {
         color: '#4B5563',
