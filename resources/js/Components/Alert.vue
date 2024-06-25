94f3e18ad28a1ea9e704b5fdfc5fc480fb3e9d35
TonyHu <benabcd5779@gmail.com>
2024-06-26 00:13:41 +0800

    feat: 新增 Alert Component

---
+++ resources/js/Components/Alert.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-if="$page.props.flash.success !== null" class="alert alert-success">
+        <Icon :style="'mr-1'" icon="heroicons-outline:check-circle"  />
+        {{ $page.props.flash.success }}
+    </div>
+    <div v-else-if="$page.props.flash.error !== null" class="alert alert-error">
+        <Icon :style="'mr-1'" icon="heroicons-outline:x-circle" />
+        {{ $page.props.flash.error }}
+    </div>
+</template>
+
+
+<script setup>
+import Icon from "@/Components/Icon.vue";
+</script>
