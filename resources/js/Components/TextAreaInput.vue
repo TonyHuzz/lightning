6bc02b4fb67241d8f91896267f3f7fe2c2ef53a5
TonyHu <benabcd5779@gmail.com>
2024-06-26 00:14:12 +0800

    feat: 新增 TextAreaInput Component

---
+++ resources/js/Components/TextAreaInput.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <label v-if="label" class="form-label" :for="id">{{ label }}：</label>
+        <textarea
+            :id="id"
+            class="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border focus:outline-none focus:shadow-outline focus:border-purple-500"
+            ref="input"
+            v-bind="$attrs"
+            v-model="inputModel"
+            :class="{ error }"
+        />
+        <div v-if="error" class="form-error">{{ error }}</div>
+    </div>
+</template>
+
+<script setup>
+
+import {onMounted, ref} from "vue";
+
+defineProps({
+    id: {
+        type: String,
+        default: () => `textarea-input-${Math.random().toString(36).substring(2, 9)}`
+    },
+    label: String,
+    error: String,
+});
+
+const inputModel = defineModel({
+    type: String,
+    required: true,
+});
+
+const input = ref(null);
+
+onMounted(() => {
+    if (input.value.hasAttribute('autofocus')) {
+        input.value.focus();
+    }
+});
+
+</script>
