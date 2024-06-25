09d351539bb8f93ca3749e4c74a9b37e755abf61
TonyHu <benabcd5779@gmail.com>
2024-06-26 00:12:52 +0800

    feat: model 定義 password & avatar 寫入資料庫的方式

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,10 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -60,5 +63,17 @@ class User extends Authenticatable
                 $user->avatar = $user->getAvatarUrl();
             }
         });
+    }
+
+    public function setPasswordAttribute($value): void
+    {
+        $this->attributes['password'] = Hash::needsRehash($value) ? Hash::make($value) : $value;
+    }
+
+    public function setAvatarAttribute($avatar): void
+    {
+        $this->attributes['avatar'] = $avatar instanceof UploadedFile
+            ? Storage::url($avatar->store('avatars'))
+            : $avatar;
     }
 }
