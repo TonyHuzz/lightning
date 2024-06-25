118d015e47fa796d6ca76a476467e0b5c9a05a40
TonyHu <benabcd5779@gmail.com>
2024-06-26 00:13:26 +0800

    feat: 定義用戶帳號設定 route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -26,14 +27,15 @@ Route::get('about', function () {
     return Inertia::render('About');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::get('user/setting', [UserController::class, 'edit'])->name('user.edit');
+    Route::put('user', [UserController::class, 'update'])->name('user.update');
 });
 
 require __DIR__.'/auth.php';
