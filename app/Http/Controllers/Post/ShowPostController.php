0b2e6ee5d2611785384302136896c36dc3f05e45
TonyHu <benabcd5779@gmail.com>
2024-06-28 23:35:01 +0800

    feat: 調整 route & 新增 show post controller

---
+++ app/Http/Controllers/Post/ShowPostController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use App\Presenters\PostPresenter;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class ShowPostController extends Controller
+{
+    public function __invoke(Post $post): Response
+    {
+        // 驗證權限
+        $this->authorize('view', $post);
+
+        $this->incrementVisits($post);
+
+        return Inertia::render('Post/Show', [
+            'post' => PostPresenter::make($post)->preset('show'),
+        ]);
+    }
+
+    private function incrementVisits(Post $post): void
+    {
+        if (auth()?->user()?->can('view', $post) && !session("posts:visited:{$post->id}")) {
+            $post->increment('visits');
+            session()->put("posts:visited:{$post->id}", true);
+        }
+    }
+}
--- routes/backend/auth/post.php
+++ routes/backend/auth/post.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Post\PostController;
+use App\Http\Controllers\Post\ShowPostController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('posts', PostController::class);
+Route::resource('posts', PostController::class)->except(['show']);
+Route::get('posts/{post}', ShowPostController::class)->name('posts.show');
