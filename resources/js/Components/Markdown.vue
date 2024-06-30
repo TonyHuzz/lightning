1b0eae0a60d5388a9092d108c9fe8a09a11ee0aa
TonyHu <benabcd5779@gmail.com>
2024-06-30 15:59:44 +0800

    feat: 讓 Markdown code block 高亮顯示

--- resources/js/Components/Markdown.vue
+++ resources/js/Components/Markdown.vue
@@ -8,7 +8,11 @@ import MarkdownIt from 'markdown-it';
 import insert from 'markdown-it-ins';
 import taskLists from 'markdown-it-task-lists';
 import hljs from 'highlight.js';
-import {computed} from "vue";
+import hljsDefineVue from 'highlightjs-vue';
+import {computed, onMounted} from "vue";
+
+// 讓Markdown code block有顏色變化
+hljsDefineVue(hljs);
 
 const props = defineProps({
     value: {
@@ -42,4 +46,9 @@ const renderedContent = computed(() => {
 
     return markdown.render(props.value);
 })
+
+// 在組件掛載時，將所有的程式碼塊進行高亮處理
+onMounted(() => {
+    hljs.highlightAll();
+});
 </script>
